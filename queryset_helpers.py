6fd2ac38172b2f1ba8ecbcc201453dc24aa60564
nickmvincent <nickmvincent@gmail.com>
2017-08-31 11:01:55 +0200

    try  this

--- queryset_helpers.py
+++ queryset_helpers.py
@@ -72,9 +72,10 @@ def list_reddit_specific_features():
     return textual + [
         'user_comment_karma', 'user_link_karma', 
         'user_is_mod', 'user_is_suspended', 'user_is_deleted',
-        'in_todayilearned', 'in_borntoday',
-        'in_wikipedia', 'in_CelebrityBornToday',
-        'in_The_Donald',
+        'in_todayilearned',
+        # 'in_borntoday',
+        # 'in_wikipedia', 'in_CelebrityBornToday',
+        # 'in_The_Donald',
     ]
 
 def list_stack_specific_features():
