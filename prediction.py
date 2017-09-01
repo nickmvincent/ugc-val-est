7c5dcd55b5d662c55e8b8b013ad886209e741e2f
nickmvincent <nickmvincent@gmail.com>
2017-09-01 10:56:39 +0200

    TRY

--- prediction.py
+++ prediction.py
@@ -293,16 +293,13 @@ def causal_inference(
         big_ndifs_counts.append(causal.summary_stats['num_large_ndiffs'])
 
         skip_fields = [
-            'in_todayilearned', 
-            'in_borntoday', 'in_wikipedia', 'in_CelebrityBornToday','in_The_Donald',
+#            'in_todayilearned', 
+#            'in_borntoday', 'in_wikipedia', 'in_CelebrityBornToday','in_The_Donald',
             'question_score',
             'num_other_answers',
             'year2008', 'year2009', 'year2010',
             'year2011', 'year2012', 'year2013',
             'year2014', 'year2015',
-            'mon', 'tues', 'wed', 'thurs',
-            'fri', 'sat',
-            'user_is_mod', 'user_is_suspended', 'user_is_deleted',
         ]
         
         if paired_psm:
