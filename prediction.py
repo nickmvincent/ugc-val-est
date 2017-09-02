2d2b3b7ccbf0cc81fc5c44f5c9c6baa23c92403c
nickmvincent <nickmvincent@gmail.com>
2017-09-02 22:58:00 +0200

    TEST

--- prediction.py
+++ prediction.py
@@ -262,8 +262,8 @@ def causal_inference(
         out.append(str(causal.summary_stats))
         ndifs.append(causal.summary_stats['sum_of_abs_ndiffs'])
         big_ndifs_counts.append(causal.summary_stats['num_large_ndiffs'])
-        causal.est_via_ols()
-        times.append(mark_time('est_via_ols'))
+        # causal.est_via_ols()
+        # times.append(mark_time('est_via_ols'))
         if not quad_psm:
             causal.est_propensity(successful_fields, exclude_from_ps)
             times.append(mark_time('propensity'))
