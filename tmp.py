415e418318afd3e22fdcfdb65dd7ed2b085d27bf
nickmvincent <nickmvincent@gmail.com>
2017-09-14 19:32:16 -0500

    tset

--- tmp.py
+++ tmp.py
@@ -1,6 +1,19 @@
 def tmp():
-    from portal.models import Revision
-    qs = Revision.objects.all().order_by('?')[:50]
-    for rev in qs:
-        print(qs.user)
-        print(qs.registration)
+    from portal.models import SampledRedditThread
+    from portal.models import SampledStackOverflowPost
+
+    qs_r = SampledRedditThread.objects.filter(has_wiki_link=True)[:5]
+    qs_s = SampledStackOverflowPost.objects.filter(has_wiki_link=True)[:5]
+    for obj in qs_r:
+        print(obj.num_edits_prev_week, obj.num_edits, obj.timestamp)
+        for link in obj.wiki_links.all():
+            print(link.url)
+        print('---')
+        input()
+    for obj in qs_s:
+        print(obj.num_edits_prev_week, obj.num_edits, obj.timestamp)
+        for link in obj.wiki_links.all():
+            print(link.url)
+        print('---')
+        input()
+tmp()
