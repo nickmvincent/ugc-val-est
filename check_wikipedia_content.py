64ae42e030d84807243967a0af47986298aebabf
nickmvincent <nickmvincent@gmail.com>
2017-09-15 11:57:34 -0500

    asd

--- check_wikipedia_content.py
+++ check_wikipedia_content.py
@@ -118,6 +118,8 @@ def make_mediawiki_request(session, base, params, verbose=False):
             break
         if 'rvlimit' in req and req['rvlimit'] == 1:
             break
+        if 'uclimit' in req and req['uclimit'] == 1:
+            break
         # if 'end' not in req:
         #     break
         last_continue = result['continue']
