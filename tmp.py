0f686b20c2714e0ac1dc31e9011c6cb12592bc69
nickmvincent <nickmvincent@gmail.com>
2017-09-15 15:41:51 -0500

    asd

--- tmp.py
+++ tmp.py
@@ -16,7 +16,8 @@ def tmp():
 def tmp2():
     from portal.models import SampledRedditThread, WikiLink, Revision, get_closest_to
     import datetime
-    qs = SampledRedditThread.objects.filter(day_of_avg_score__isnull=True)
+    qs = SampledRedditThread.objects.filter(has_wiki_link=True,day_of_avg_score__isnull=True)
+    count = 0
     for post in qs:
         for link_obj in post.wiki_links.all():
             all_possible_links = WikiLink.objects.filter(title=link_obj.title)
@@ -24,13 +25,20 @@ def tmp2():
                 'day_of': post.timestamp,
                 'week_after': post.timestamp + datetime.timedelta(days=7),
             }
+            missing_necessary_ores = False
+            revs = []
             for field, dt in field_to_dt.items():
-                ores_rev = get_closest_to(
+                rev = get_closest_to(
                     Revision.objects.filter(
                         wiki_link__in=all_possible_links), dt)
-                if ores_rev:
-                    rev_as_qs = Revision.objects.filter(revid=ores_rev.revid).values()[0]
-                    print(rev_as_qs)
-                else:
-                    print('No ores_Rev')
+                ores_score = rev.score
+                revs.append(rev.revid)
+                if ores_score is None:
+                    missing_necessary_ores = True
+                count += 1
+            if missing_necessary_ores:
+                print(revs)
+
+            if count >= 10:
+                break
                     
