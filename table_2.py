0424d284781f1ab475e15d75e6297d367c073d51
nickmvincent <nickmvincent@gmail.com>
2017-12-15 14:00:25 -0600

    simplify

---
+++ table_2.py
@@ -0,0 +1,36 @@
+import numpy as np
+from scipy import stats
+
+def main():
+    """
+    Main method
+    """
+    kwargs = {
+        'has_wiki_link': True,
+        'day_of_avg_score__isnull': False,
+        'week_after_avg_score__isnull': False,
+    }
+    qsr = SampledRedditThread.objects.filter(**kwargs)
+    qss = SampledStackOverflowPost.objects.filter(**kwargs)
+
+    for var in [
+        'num_edits', 'num_edits_prev_week'
+    ]:
+        vals_r = qsr.values_list(var, flat=True)
+        vals_s = qss.values_list(var, flat=True)
+        print('Variable is', var)
+        print('Mean for Reddit is', np.mean(vals_r))
+        print('Mean for SO is', np.mean(vals_s))
+
+
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dja.settings")
+    import django
+    from django.db import connection
+    django.setup()
+    from portal.models import (
+        SampledRedditThread, SampledStackOverflowPost,
+        StackOverflowAnswer
+    )
+    main()
