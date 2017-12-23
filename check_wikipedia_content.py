6396a336b51142a39cbc248416222599370bfe5a
nickmvincent <nickmvincent@gmail.com>
2017-12-22 16:07:45 -0800

    get scores only for R and S

--- check_wikipedia_content.py
+++ check_wikipedia_content.py
@@ -845,7 +845,8 @@ def parse():
         else:
             test()
     elif args.get_scores_only:
-        get_scores_only(SampledStackOverflowPost)
+        for model in [SampledRedditThread, SampledStackOverflowPost]
+            get_scores_only(model)
     elif args.recalc_pageviews:
         posts = SampledRedditThread.objects.filter(has_wiki_link=True, sample_num__in=[0,1,2],
            # timestamp__gte=datetime.datetime(year=2015, month=7, day=1),
