178b775463476b3775b01aa7dcda3fdc70227730
nickmvincent <nickmvincent@gmail.com>
2017-12-23 19:55:12 -0800

    update batch file!

--- batch.sh
+++ batch.sh
@@ -1,8 +1,10 @@
-python prediction.py
+python prediction.py --rq 1
+python prediction.py --rq 2
+
 python prediction.py --platform s --rq 1-is_top
 python prediction.py --platform s --rq 2-is_top
 
-python stats.py --rq 1
+python stats.py --rq 10 --frequency
 python stats.py --platform s --rq 13
 python stats.py --platform s --rq 14
 python stats.py --platform s --rq 33
