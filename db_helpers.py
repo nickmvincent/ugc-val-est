ab9cbedf779c24d27dbe5c17944f9c17d6064a26
nickmvincent <nickmvincent@gmail.com>
2017-12-30 13:32:13 -0600

    asd

--- db_helpers.py
+++ db_helpers.py
@@ -237,31 +237,11 @@ def mark_top_answers():
 
 
 def quick_helper():
-    from portal.models import SampledRedditThread, WikiLink, Revision, get_closest_to
-    import datetime
-    qs = SampledRedditThread.objects.filter(has_wiki_link=True,day_of_avg_score__isnull=True)
-    for post in qs:
-        post.save()
-        print(post.day_of_avg_score)
-        for link_obj in post.wiki_links.all():
-            all_possible_links = WikiLink.objects.filter(title=link_obj.title)
-            field_to_dt = {
-                'day_of': post.timestamp,
-                'week_after': post.timestamp + datetime.timedelta(days=7),
-            }
-            missing_necessary_ores = False
-            revs = []
-            for field, dt in field_to_dt.items():
-                rev = get_closest_to(
-                    Revision.objects.filter(
-                        wiki_link__in=all_possible_links), dt)
-                ores_score = rev.score
-                revs.append((rev.revid, rev.score))
-                if ores_score is None:
-                    missing_necessary_ores = True
-                if missing_necessary_ores:
-                    print(revs)
-        input()
+    links = WikiLink.objects.all().order_by('?')[:10]
+    for link in links:
+        print(url)
+        print(title)
+        print(alt_title)
     
     
 
