7540434bc5783bf5b4ae427643d8b072f62ef8c3
nickmvincent <nickmvincent@gmail.com>
2018-03-01 14:13:42 -0600

    doing a quick comparison test

--- tmpscript.py
+++ tmpscript.py
@@ -1,18 +1,7 @@
 def tmp():
-    from portal.models import SampledRedditThread, WikiLink, Revision
-    from django.db import models
-    import datetime
-    thread = SampledRedditThread.objects.get(uid='3z09iu')
-    for link_obj in thread.wiki_links.all():
-        print(link_obj.title)
-        all_possible_links = WikiLink.objects.filter(
-            models.Q(title__in=[link_obj.title, link_obj.alt_title]) | models.Q(alt_title__in=[link_obj.title, link_obj.alt_title])
-        )
-        starttime = thread.timestamp - datetime.timedelta(days=7)
-        endtime = thread.timestamp + datetime.timedelta(days=7)
-        revisions = Revision.objects.filter(
-            wiki_link__in=all_possible_links,
-            timestamp__gte=starttime, timestamp__lte=endtime)
-        print(revisions)
-        for rev in revisions:
-            print(rev.score)
+    from portal.models import SampledRedditThread
+    noneng = SampledRedditThread.objects.filter(has_wiki_link=True).exclude(url__contains='en.wikipedia').exclude(url__contains='en.m.wikipedia').exclude(url__contains='www.wikipedia').exclude(url__contains='//wikipedia')
+    print(noneng.count())
+    for x in noneng:
+        print(x.url)
+        input()
